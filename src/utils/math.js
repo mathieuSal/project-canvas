dddfad09421f99488fb979698c6efd8e09ab1d10
Mathieu Salles <mathieu.sal@gmail.com>
2021-10-13 13:54:33 +0200

    feat(pc): change in math.js
    Will have to fix that with typescript

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,7 +1,7 @@
-
+"use strict";
 exports.__esModule = true;
 exports.sum = void 0;
-const sum = function (a, b) {
+var sum = function (a, b) {
     return parseFloat(a) + parseFloat(b);
 };
 exports.sum = sum;
